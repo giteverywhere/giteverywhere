0255c218277eb7604bc1c13268bf8460bfa9d370
Kashif Iftikhar <kashif@compulife.com.pk>
2013-04-18 21:57:11 +0500

    removed the bad project name for pyck

--- module1/module1/apps/__init__.py
+++
@@ -1,8 +0,0 @@
-# This list contains all the enabled apps for the project, when you add a new app, add it to this list
-# to make it accessible by the project.
-
-import importlib
-
-enabled_apps = []
-PROJECT_NAME = 'module1'
-project_package = importlib.import_module("module1")
